9e31b84148b76ada23d875d74c937e1a4dda5a1e
Arlez <rainmake123@gmail.com>
2025-08-20 10:37:55 -0400

    se agrego IA al proyecto

--- src/stores/aiSlice.ts
+++ src/stores/aiSlice.ts
@@ -3,12 +3,21 @@ import AIService from '../services/AIService'
 
 export type AISlice = {
     recipe: string
+    isGenerating: boolean
     generateRecipe: (prompt: string) => Promise<void>
 }
 
-export const createAISlice : StateCreator<AISlice, [], [], AISlice> = () => ({
+export const createAISlice : StateCreator<AISlice> = (set) => ({
     recipe: '',
+    isGenerating: false,
     generateRecipe: async (prompt) => {
-        await AIService.generateRecipe(prompt)
+        set({recipe: '', isGenerating: true})
+        const data = await AIService.generateRecipe(prompt)
+        for await (const textPart of data){
+            set((state=>({
+                recipe: state.recipe + textPart
+            }))) 
+        }
+        set({isGenerating: false})
     }
 })
